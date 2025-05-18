762e04307957db83f78975b9f18961f1f3fa147a
SpamSms2 <87837018+SpamSms2@users.noreply.github.com>
2025-05-19 04:42:51 +0800

    Update  bash.py

--- bash.py
+++ bash.py
@@ -1,76 +1,95 @@
 #!/bin/python
 
-import os,sys,time
+import os
+import sys
+import time
+
 def sp(a):
-  for e in a + "\n":
-    sys.stdout.write(e)
-    sys.stdout.flush()
-    time.sleep(0.002)
+    for e in a + "\n":
+        sys.stdout.write(e)
+        sys.stdout.flush()
+        time.sleep(0.002)
+
 def lagi():
-    f = input("Do You Want To Continue ? [y/n] : ")
-    if f == "y":
-        os.system("python bash.py")
-    elif f == "n":
-         sys.exit()
-
-
+    while True:
+        f = input("Do You Want To Continue ? [y/n] : ").lower()
+        if f == "y":
+            menu()
+            break
+        elif f == "n":
+            print("Terima kasih! Bye.")
+            sys.exit()
+        else:
+            print("Input tak sah. Sila cuba lagi.")
 
 def bersih():
-   os.system("clear")
+    os.system("clear")
 
 def menu():
     bersih()
-    print ("\033[1;96m+---------------------------------------+")
-    print ("Author : R14N")
-    print ("Youtube : R14N")
-    print ("Instagram : riangaming_yt")
-    print ("Partner : Mr.1557") 
-    print ("+---------------------------------------+")
-    print ("\033[1;92m[1]Install Metasploit")
-    print ("[2]Spam Call (by Mr.1557)")
-    print ("[3]Hack CCTV")
-    print ("[4]Tools Instaler payload no root")
-    print ("[5]IP Tracker")
-    print ("[6]Deface website")
-    print ("[7]DDos attack")
-    print ("\033[1;91m[B]Back/Keluar")
-    pil = input ("\033[1;95mPilih Sesuai Nomor -> ")
-    if pil =="1":
-       os.system("pkg update && pkg upgrade")
-       os.system("pkg install git")
-       os.system("wget https://raw.githubusercontent.com/gushmazuko/metasploit_in_termux/master/metasploit.sh")
-       os.system("chmod +x metasploit.sh")
-       os.system("./metasploit.sh")
-       os.system("./postgresql_ctl.sh start")
-       os.system("msfconsole")
-       lagi()
-    elif pil =="2":
-       os.system("pkg update && pkg upgrade")
-       os.system("pkg install git")
-       os.system("pkg install python")
-       os.system("git clone https://github.com/Aldi4321/spam-call")
-       lagi()
-    elif  pil =="3":
-       os.system("pkg update && pkg upgrde")
-       os.system("termux-setup-storage")
-       os.system("git clone https://github.com/AngelSecurityTeam/Cam-Hackers")
-       lagi()
-    elif pil =="4":
-       os.system("git clone https://github.com/TechnicalMujeeb/tmvenom")
-       lagi()
-    elif pil =="5":
-       os.system("git clone git://github.com/htr-tech/track-ip.git")
-       lagi()
-    elif pil =="6":
-        print ("next update bro")
-    elif pil =="7":
-        print ("next update bro")
-    elif pil =="B":
-       lagi()
-       sys.exit()
-    else:
-       print ("salah bang")
-       time.sleep(1)
-       os.system("python bash.py")
-menu()
+    print("\033[1;96m+---------------------------------------+")
+    print("Author : R14N")
+    print("Youtube : R14N")
+    print("Instagram : riangaming_yt")
+    print("Partner : Mr.1557")
+    print("+---------------------------------------+")
+    print("\033[1;92m[1] Install Metasploit")
+    print("[2] Spam Call (by Mr.1557)")
+    print("[3] Hack CCTV")
+    print("[4] Tools Installer payload no root")
+    print("[5] IP Tracker")
+    print("[6] Deface website")
+    print("[7] DDos attack")
+    print("\033[1;91m[B] Back/Keluar")
+    pil = input("\033[1;95mPilih Sesuai Nomor -> ").lower()
 
+    if pil == "1":
+        os.system("pkg update && pkg upgrade -y")
+        os.system("pkg install git -y")
+        os.system("wget https://raw.githubusercontent.com/gushmazuko/metasploit_in_termux/master/metasploit.sh")
+        os.system("chmod +x metasploit.sh")
+        os.system("./metasploit.sh")
+        os.system("./postgresql_ctl.sh start")
+        os.system("msfconsole")
+        lagi()
+
+    elif pil == "2":
+        os.system("pkg update && pkg upgrade -y")
+        os.system("pkg install git -y")
+        os.system("pkg install python -y")
+        os.system("git clone https://github.com/Aldi4321/spam-call")
+        lagi()
+
+    elif pil == "3":
+        os.system("pkg update && pkg upgrade -y")  # betulkan typo upgrde -> upgrade
+        os.system("termux-setup-storage")
+        os.system("git clone https://github.com/AngelSecurityTeam/Cam-Hackers")
+        lagi()
+
+    elif pil == "4":
+        os.system("git clone https://github.com/TechnicalMujeeb/tmvenom")
+        lagi()
+
+    elif pil == "5":
+        os.system("git clone git://github.com/htr-tech/track-ip.git")
+        lagi()
+
+    elif pil == "6":
+        print("Next update bro")
+        lagi()
+
+    elif pil == "7":
+        print("Next update bro")
+        lagi()
+
+    elif pil == "b":
+        print("Keluar program. Terima kasih.")
+        sys.exit()
+
+    else:
+        print("Salah pilihan, cuba lagi.")
+        time.sleep(1)
+        menu()
+
+if __name__ == "__main__":
+    menu()
